a0fcc5477ceb3592729631b80232c71a5be60429
kmenzli <kmenzli@exoplatform.com>
2012-11-05 20:42:38 +0100

    PLF-3858 : Implement help portlet in the top navigation bar

---
+++ component/UXPnavigation/src/main/java/org/exoplatform/platform/navigation/component/help/impl/HelpServiceImpl.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (C) 2012 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.platform.navigation.component.help.impl;
+
+import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.container.xml.PropertiesParam;
+import org.exoplatform.platform.navigation.component.help.HelpService;
+import org.exoplatform.platform.navigation.component.help.Helper;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <a href="kmenzli@exoplatform.com">Kmenzli</a>
+ * @date 05/11/12
+ */
+public class HelpServiceImpl implements HelpService {
+
+    private static final Log LOG = ExoLogger.getExoLogger(HelpServiceImpl.class);
+
+    PropertiesParam param = null;
+
+    private final PropertiesParam props;
+
+    public HelpServiceImpl(InitParams params) {
+
+        if (params == null) {
+            //TODO create new exception and throw it
+        }
+
+        this.props = params.getPropertiesParam("help.pages");
+
+        if (props == null)
+        {
+            // TODO create new Exception 2 and throw it
+        }
+    }
+
+    public String fetchHelpPage(String currentNavigation) {
+
+        String helpPage = Helper.DEFAULT_HELP_PAGE;
+
+        if (props != null) {
+
+                helpPage = props.getProperty(currentNavigation);
+
+        }
+        if (!Helper.present(helpPage)) {
+
+            return     getDefaultPageHelp();
+
+        }
+
+        return helpPage;
+    }
+
+    public String getDefaultPageHelp () {
+
+        if (props != null) {
+
+            if (Helper.present(props.getProperty("default"))) {
+                 return props.getProperty("default") ;
+            }
+
+        }
+        return Helper.DEFAULT_HELP_PAGE;
+
+    }
+
+
+}
--- extension/portlets/platformNavigation/src/main/java/org/exoplatform/platform/component/UIHelpPlatformToolbarPortlet.java
+++ extension/portlets/platformNavigation/src/main/java/org/exoplatform/platform/component/UIHelpPlatformToolbarPortlet.java
@@ -1,53 +1,81 @@
+/**
+ * Copyright (C) 2012 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.exoplatform.platform.component;
 
 
-import org.exoplatform.container.ExoContainer;
-import org.exoplatform.container.ExoContainerContext;
-import org.exoplatform.navigation.bar.help.HelpPortletUtils;
-import org.exoplatform.navigation.bar.help.HelpService;
+import org.exoplatform.platform.navigation.component.help.HelpService;
+import org.exoplatform.platform.navigation.component.help.Helper;
 import org.exoplatform.portal.webui.util.Util;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.core.UIPortletApplication;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
 
 /**
- * Created with IntelliJ IDEA.
- * User: fatma
- * Date: 01/11/12
- * Time: 10:08
- * To change this template use File | Settings | File Templates.
+ * @author <a href="kmenzli@exoplatform.com">Kmenzli</a>
+ * @date 05/11/12
  */
+
 @ComponentConfig(lifecycle = UIApplicationLifecycle.class, template = "app:/groovy/platformNavigation/portlet/UIHelpPlatformToolbarPortlet/UIHelpPlatformToolbarPortlet.gtmpl")
 public class UIHelpPlatformToolbarPortlet extends UIPortletApplication {
 
-    private String currentNavigation = null;
+    private static final Log LOG = ExoLogger.getExoLogger(UIHelpPlatformToolbarPortlet.class);
+    private String currentNavigation = "";
+    private String helpPage = "";
     private HelpService helpService = null;
 
     public UIHelpPlatformToolbarPortlet() throws Exception {
-
-    }
-
-    public void getCurrentNavigation() {
-        try {
-            currentNavigation = Util.getUIPortal().getNavPath().getName();
-            if (currentNavigation == null)   currentNavigation= "default.help.page";
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        helpService = getApplicationComponent(HelpService.class);
     }
 
     public String getHelpPage() {
 
         getCurrentNavigation();
-        //helpService = getApplicationComponent(HelpService.class);
 
-        ExoContainer myContainer = ExoContainerContext.getCurrentContainer();
-        helpService = (HelpService) myContainer.getComponentInstanceOfType(HelpService.class);
+        //TODO use a Helper Class for this test
+        if (currentNavigation != null && currentNavigation.length() !=0) {
 
+            helpPage = helpService.fetchHelpPage(currentNavigation);
 
-        if (helpService == null) {
-            return HelpPortletUtils.DEFAULT_HELP_PAGE;
         }
-        else return helpService.fetchHelpPage(currentNavigation);
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("The navigation [[ "+currentNavigation+" ]] has as page help [[ "+helpPage+" ]]");
+        }
+
+        return  helpPage;
+    }
+
+    //TODO put it in helper class
+    private void getCurrentNavigation() {
+        try {
+
+            currentNavigation =  Util.getUIPortal().getNavPath().getName();
+
+        } catch (Exception E) {
+
+            LOG.warn("Can not load the currentNavigation ",E);
+
+        }
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("The current Navigation is [[ "+currentNavigation+" ]]");
+        }
     }
 }
