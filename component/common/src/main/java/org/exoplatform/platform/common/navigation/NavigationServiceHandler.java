f628ad70455e970cd2f34a9d05b4ca92c964748b
Hela Zekri <hzekri@exoplatform.com>
2012-11-29 16:29:31 +0100

    PLF-3901 [Top Navigation][Home Portlet] Load company logo dynamically from JCR (logo recovery)

--- component/common/src/main/java/org/exoplatform/platform/common/navigation/NavigationServiceHandler.java
+++ component/common/src/main/java/org/exoplatform/platform/common/navigation/NavigationServiceHandler.java
@@ -18,10 +18,18 @@
  */
 package org.exoplatform.platform.common.navigation;
 
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
+import org.exoplatform.services.jcr.ext.hierarchy.NodeHierarchyCreator;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.services.wcm.core.BaseWebSchemaHandler;
 
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -31,6 +39,7 @@ import java.util.GregorianCalendar;
  */
 public class NavigationServiceHandler extends BaseWebSchemaHandler {
 
+    private static Log logger = ExoLogger.getLogger(NavigationServiceHandler.class);
     Calendar calendar = new GregorianCalendar();
 
     public void onCreateNode(SessionProvider sessionProvider, final Node portalFolder) throws Exception {
@@ -39,7 +48,7 @@ public class NavigationServiceHandler extends BaseWebSchemaHandler {
             Node applicationDataFolder = portalFolder.getNode("ApplicationData");
             Node logoApplicationFolder = applicationDataFolder.addNode("logo", NT_UNSTRUCTURED);
             addMixin(logoApplicationFolder, "exo:owneable");
-            addMixin(logoApplicationFolder,"exo:datetime");
+            addMixin(logoApplicationFolder, "exo:datetime");
             addMixin(logoApplicationFolder, "exo:hiddenable");
             logoApplicationFolder.setProperty("exo:dateCreated", calendar);
 
@@ -48,6 +57,58 @@ public class NavigationServiceHandler extends BaseWebSchemaHandler {
         portalFolder.getSession().save();
     }
 
+    public static String getHomePageLogoURI() {
+        Boolean isavailable = false;
+        Node imageNode = null;
+        String pathImageNode = null;
+        try {
+            NodeHierarchyCreator nodeCreator = (NodeHierarchyCreator) ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(NodeHierarchyCreator.class);
+            SessionProvider sProvider = SessionProvider.createSystemProvider();
+            Node publicApplicationNode = nodeCreator.getPublicApplicationNode(sProvider);
+            Session session = publicApplicationNode.getSession();
+            Node rootNode = session.getRootNode();
+            String portalName = Util.getPortalRequestContext().getPortalOwner();
+            String path = "sites content/live/" + portalName + "/ApplicationData/logo/";
+            Node logoNode = rootNode.getNode(path);
+            if (logoNode.hasNodes()) {
+                for (NodeIterator iterator = logoNode.getNodes(); iterator.hasNext(); ) {
+                    Node node = iterator.nextNode();
+                    imageNode = node;
+                    if (imageNode.hasNode("jcr:content")) {
+                        if (imageNode.getNode("jcr:content").hasProperty("jcr:mimeType")) {
+                            String JcrMimeType = imageNode.getNode("jcr:content").getProperty("jcr:mimeType").getString();
+                            if (JcrMimeType.equals("image/gif") || JcrMimeType.equals("image/ief") ||
+                                    JcrMimeType.equals("image/jpeg") || JcrMimeType.equals("image/pjpeg") ||
+                                    JcrMimeType.equals("image/bmp") || JcrMimeType.equals("image/x-portable-bitmap") ||
+                                    JcrMimeType.equals("image/x-portable-graymap") || JcrMimeType.equals("image/png") ||
+                                    JcrMimeType.equals("image/x-png") || JcrMimeType.equals("image/x-portable-anymap") ||
+                                    JcrMimeType.equals("image/x-portable-pixmap") || JcrMimeType.equals("image/x-cmu-raster") ||
+                                    JcrMimeType.equals("image/x-rgb") || JcrMimeType.equals("image/tiff") ||
+                                    JcrMimeType.equals("image/x-xbitmap") || JcrMimeType.equals("image/x-xpixmap") ||
+                                    JcrMimeType.equals("image/x-xwindowdump")) {
+
+                                isavailable = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Get logo : Can not Find node", e);
+        }
+        if (isavailable == true) {
+            try {
+                pathImageNode = imageNode.getPath();
+            } catch (RepositoryException e) {
+                logger.error("Get logo : Can not Find the path of node", e);
+            }
+            return pathImageNode;
+        } else
+            return null;
+    }
+
+
     @Override
     protected String getHandlerNodeType() {
         return "exo:portalFolder";
@@ -55,6 +116,6 @@ public class NavigationServiceHandler extends BaseWebSchemaHandler {
 
     @Override
     protected String getParentNodeType() {
-         return "nt:unstructured";
+        return "nt:unstructured";
     }
 }
